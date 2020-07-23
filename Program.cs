26b92c5c42031d008ef686eb59c0373746e00ab8
QiQiQiQiQ <40237626+QiQiQiQiQ@users.noreply.github.com>
2020-07-23 10:17:56 +0200

    23.07.2020

--- Program.cs
+++ Program.cs
@@ -32,7 +32,8 @@ namespace Sniffing
         public const byte STOP_REQ = 0x02;
         public const byte EXIT_REQ = 0x03;
         public const byte DATA_ACK = 0x04;
-        public const byte STOP_ACK = 0x05;
+        //public const byte STOP_ACK = 0x05;
+        public const byte ERROR = 0x05;
         public const byte DATA = 0x10;
         public const byte GLOBAL_HEADER = 0x11;
         public const UInt32 magic_number_swapped = 0xd4c3b2a1;
@@ -61,7 +62,7 @@ namespace Sniffing
     }
     class USER_COMMAND
     {
-        public const ConsoleKey REQ_ALL = ConsoleKey.A;
+        public const ConsoleKey REQ_ALL = ConsoleKey.A; // not working yet, not used in this programm
         public const ConsoleKey REQ_EV_EVSE = ConsoleKey.E;
         public const ConsoleKey EXIT = ConsoleKey.X;
         public const ConsoleKey STOP = ConsoleKey.S;
@@ -134,6 +135,7 @@ namespace Sniffing
         // general buffers
         private static byte[] buf_send = new byte[256];
         private static byte[] buf_receive = new byte[10000];
+        private static byte[] buf_error = new byte[3000];   // fetch all datastream in the socket on error, so that process don't block
         // buffers for for individual fields
         private static byte[] buf_control_field = new byte[4];
         private static byte[] buf_length_field = new byte[5];
@@ -147,6 +149,8 @@ namespace Sniffing
         private static UInt16 len_radiotap;
         private static byte len_MAC;
         private static UInt16 len_frame_body;
+
+        private static UInt16 Error_cnt = 0; // count errors on receiving frames
 
         static void Main(string[] args)
         {
@@ -175,21 +179,20 @@ namespace Sniffing
                     if(Console.KeyAvailable == true)
                     {
                         cki = Console.ReadKey(true);
-                        if (cki.Key == USER_COMMAND.REQ_ALL)
+                        
+                        if (cki.Key == USER_COMMAND.STOP)
                         {
-                            Console.WriteLine("request all");
-                            Send_to_client(FTYPE.SEND_REQ, FLAG.ALL, null);
-                            break;
+                            Console.WriteLine("\nprogram not running");
                         }
                         else if (cki.Key == USER_COMMAND.REQ_EV_EVSE)
                         {
-                            Console.WriteLine("request EV/EVSE frames");
+                            Console.WriteLine("\nrequest EV/EVSE frames");
                             Send_to_client(FTYPE.SEND_REQ, FLAG.EV_EVSE, null);
                             break;
                         }
                         else if (cki.Key == USER_COMMAND.EXIT)
                         {
-                            Console.WriteLine("conncetion shutdown, press any key to exit program...");
+                            Console.WriteLine("\nconncetion shutdown, press any key to exit program...");
                             Send_to_client(FTYPE.EXIT_REQ, FLAG.NONE, null);
                             clientSocket.Close();
                             Console.ReadKey();
@@ -197,13 +200,12 @@ namespace Sniffing
                         }
                         else
                         {
-                            Console.WriteLine("invalid key");
-                            Thread.Sleep(1000);
+                            Console.WriteLine("\ninvalid key");
                         }
                     }
                     else
                     {
-                        Thread.Sleep(1000);
+                        Thread.Sleep(200);
                     }
                 }
 
@@ -215,16 +217,19 @@ namespace Sniffing
 
         public static void Open_Socket()
         {
-            /* acquire proper ip address and port */
+            /* ask user to enter proper ip address and port of the client machine
+             * ip could be checked in cmd.exe with 'ipconfig -all'
+             * port number must be the same with that on client machine
+             */
             int port;
-            Console.WriteLine("Please enter a port number between 2000 and 65535");
+            Console.WriteLine("Please enter a port number between 49152 and 65535");
             string port_str = Console.ReadLine();
-            port = Int16.Parse(port_str);
-            while (port < 2000 || port > 65535)
+            port = Int32.Parse(port_str);
+            while (port < 49152 || port > 65535)
             {
                 Console.WriteLine("invalid port, please enter again");
                 port_str = Console.ReadLine();
-                port = Int16.Parse(port_str);
+                port = Int32.Parse(port_str);
             }
             IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
             for (int i = 1; i <= ipHost.AddressList.Length; i++)
@@ -232,8 +237,15 @@ namespace Sniffing
                 Console.WriteLine("ip {0}: {1}", i, ipHost.AddressList[i - 1].ToString());
             }
             Console.WriteLine("Please enter the index of the ip address you want to use");
+            int ip_index = 0;
             string index = Console.ReadLine();
-            int ip_index = Int16.Parse(index) - 1;
+            ip_index = Int16.Parse(index) - 1;
+            while (ip_index < 0 || ip_index >= ipHost.AddressList.Length)
+            {
+                Console.WriteLine("invalid");
+                index = Console.ReadLine();
+                ip_index = Int16.Parse(index) - 1;
+            }
             IPAddress ipAddr = ipHost.AddressList[ip_index];
             Console.WriteLine("Host ip address: {0}", ipAddr.ToString());
 
@@ -245,7 +257,7 @@ namespace Sniffing
                 /* bind socket */
                 listener.Bind(localEndPoint);
                 listener.Listen(5);
-                Console.WriteLine("Waiting connection ...");
+                Console.WriteLine("Waiting for connection ...");
                 clientSocket = listener.Accept();
             }
             catch (Exception e)
@@ -267,7 +279,7 @@ namespace Sniffing
                     if(sock.Available > 31)
                     {
                         num = sock.Receive(BUFFER.receive, 32, SocketFlags.None);
-                        Print_Content(BUFFER.receive, 0, num);
+                        //Print_Content(BUFFER.receive, 0, num);
                         continue;
                     }
                     else
@@ -276,7 +288,7 @@ namespace Sniffing
                         if(sock.Available < 32)
                         {
                             num = sock.Receive(BUFFER.receive, sock.Available, SocketFlags.None);
-                            Print_Content(BUFFER.receive, 0, num);
+                            //Print_Content(BUFFER.receive, 0, num);
                         }
                     }
                 }
@@ -287,30 +299,60 @@ namespace Sniffing
             }
         }
 
-        private static void Print_Content(byte[] field, int start, int end)
+        private static void Print_Content(byte[] field, int start, int end, ConsoleColor color)
         {
+            Console.ForegroundColor = color;
+            bool oui = false;
+            int oui_len = 0;
+            int oui_begin = end;
             if (start > end)
                 return;
             for (int i = start; i < end; i++)
             {
+                // check if OUI present
+                if (field[i] == 0xdd)
+                {
+                    if (field[i + 2] == 0x70
+                        && field[i + 3] == 0xb3
+                        && field[i + 4] == 0xd5
+                        && field[i + 5] == 0x31
+                        && field[i + 6] == 0x90)
+                    {
+                        oui_len = field[i + 1] + 2;
+                        oui_begin = i;
+                        oui = true;
+                    }
+                }
+
+                if (oui)
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    if (i - oui_begin >= oui_len)
+                    {
+                        oui = false;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                }
+
                 if (((i-start) % 8) == 0)
                 {
                     if (i == start)
-                        Console.Write("\t");
+                        Console.Write("    ");
                     else if ((i - start) % 32 == 0)
-                        Console.Write("\n\t");
+                        Console.Write("\n    ");
                     else
                         Console.Write(" ");
                 }
                 Console.Write("{0} ", field[i].ToString("x2"));
             }
+            Console.ForegroundColor = ConsoleColor.White;
             return;
         }
 
         private static void Show_Usage()
         {
             Console.WriteLine("\nEnter Command by pressing the Keyboard: ");
-            Console.Write("X - Exit, S - stop, A - request all frame, E - request frames from EV_EVSE");
+            Console.Write("X - Exit\nS - stop\nE - capture EV and EVSE frames");
             return;
         }
 
@@ -341,6 +383,10 @@ namespace Sniffing
 
         private static void Write_to_logfile(byte[] buffer)
         {
+            /* this function is not finished, the idea is that a logfile write stream is created when the program is started
+             * it can be named after the date and time
+             * this function writes the data in the buffer into the logfile in UTF-8 coded form.
+             */
             return;
         }
 
@@ -458,49 +504,53 @@ namespace Sniffing
             }
         }
 
-        private static void Wait_For_Datastream(int size, string to_display)
+        private static int Wait_For_Datastream(int size, string to_display)
         {
-            /* wait until enough data available. When interrupted by STOP command a true value will be returned */
+            /* wait until enough data available. return 0 on success, return 1 if data dont comes in 5 seconds, return 2 if stop key pressed */
             int timer = 0;
             while (clientSocket.Available < size)
             {
                 Thread.Sleep(200);
                 timer++;
-                /*
+                
                 if (Console.KeyAvailable == true)
                 {
                     ConsoleKeyInfo cki = Console.ReadKey(true);
                     if (cki.Key == USER_COMMAND.STOP)
                     {
-                        Send_to_client(clientSocket, FTYPE.STOP_REQ, FLAG.NONE, null);
-                        return true;
-                    }
-                    else if ((cki.Key == USER_COMMAND.REQ_ALL) && (req_flag != FLAG.ALL))
-                    {
-                        req_flag = FLAG.ALL;
-                        Send_to_client(clientSocket, FTYPE.SEND_REQ, FLAG.ALL, null);
-                    }
-                    else if ((cki.Key == USER_COMMAND.REQ_EV_EVSE) && (req_flag != FLAG.EV_EVSE))
-                    {
-                        req_flag = FLAG.EV_EVSE;
-                        Send_to_client(clientSocket, FTYPE.SEND_REQ, FLAG.EV_EVSE, null);
+                        return 2;
                     }
                 }
-                */
-                if (timer % 15 == 0)
+                
+                if (timer >= 25)
                 {
-                    Console.WriteLine("waiting for '{0}'...", to_display);
+                    // time exceeded 5 s, flush the socket and return error msg
+                    Error_cnt++;
+                    Array.Clear(buf_error, 0, buf_error.Length);
+                    clientSocket.Receive(buf_error, clientSocket.Available, SocketFlags.None);
+                    Console.WriteLine("\nError waiting for '{0}'..., {1} errors in total", to_display, Error_cnt);
+                    return 1;
                 }
             }
-            return;
+            return 0;
         }
 
         private static bool Process_a_frame()
         {
-            /*** receive and process one frame, if terminated by the STOP command, return true ***/
+            /*** receive and process one frame, if terminated by the STOP command, return false, else return true ***/
 
             /* receive the control field */
-            Wait_For_Datastream(4, "control field");
+            int wait_flag = Wait_For_Datastream(4, "control field");
+            if ( wait_flag == 1)
+            {
+                Send_to_client(FTYPE.ERROR, FLAG.NONE, null);
+                return true;
+            }
+            else if ( wait_flag == 2)
+            {
+                Send_to_client(FTYPE.STOP_REQ, FLAG.NONE, null);
+                return false;
+            }
             Array.Clear(buf_control_field, 0, buf_control_field.Length);
             clientSocket.Receive(buf_control_field, 4, SocketFlags.None);
             byte R_type = buf_control_field[0];
@@ -513,14 +563,25 @@ namespace Sniffing
                 Array.Clear(buf_global_hdr, 0, buf_global_hdr.Length);
                 clientSocket.Receive(buf_global_hdr, 24, SocketFlags.None);
                 Send_to_client(FTYPE.DATA_ACK, FLAG.NONE, null);
+                Console.ForegroundColor = ConsoleColor.White;
                 Console.WriteLine("**********************************************\nPCAP Global header");
-                Print_Content(buf_global_hdr, 0, 24);
+                Print_Content(buf_global_hdr, 0, 24, ConsoleColor.White);
                 Write_to_logfile(buf_global_hdr);
             }
             else if ( R_type == FTYPE.DATA )
             {
                 // length field
-                Wait_For_Datastream(5, "length field");
+                wait_flag = Wait_For_Datastream(5, "length field");
+                if (wait_flag == 1)
+                {
+                    Send_to_client(FTYPE.ERROR, FLAG.NONE, null);
+                    return true;
+                }
+                else if (wait_flag == 2)
+                {
+                    Send_to_client(FTYPE.STOP_REQ, FLAG.NONE, null);
+                    return false;
+                }
                 Array.Clear(buf_length_field, 0, buf_length_field.Length);
                 clientSocket.Receive(buf_length_field, 5, SocketFlags.None);
                 len_radiotap = BitConverter.ToUInt16(buf_length_field, 0);
@@ -528,24 +589,54 @@ namespace Sniffing
                 len_frame_body = BitConverter.ToUInt16(buf_length_field, 3);
 
                 // PCAP packet header
-                Wait_For_Datastream(LEN.PKT_HDR, "PCAP packet header " + packet_nr.ToString());
+                wait_flag = Wait_For_Datastream(LEN.PKT_HDR, "PCAP packet header " + packet_nr.ToString());
+                if (wait_flag == 1)
+                {
+                    Send_to_client(FTYPE.ERROR, FLAG.NONE, null);
+                    return true;
+                }
+                else if (wait_flag == 2)
+                {
+                    Send_to_client(FTYPE.STOP_REQ, FLAG.NONE, null);
+                    return false;
+                }
                 Array.Clear(buf_packet_hdr, 0, buf_packet_hdr.Length);
                 clientSocket.Receive(buf_packet_hdr, LEN.PKT_HDR, SocketFlags.None);
                 Write_to_logfile(buf_packet_hdr);
 
                 // radiotap header
-                Wait_For_Datastream((int)len_radiotap, "radiotap header");
+                wait_flag = Wait_For_Datastream((int)len_radiotap, "radiotap header");
+                if (wait_flag == 1)
+                {
+                    Send_to_client(FTYPE.ERROR, FLAG.NONE, null);
+                    return true;
+                }
+                else if (wait_flag == 2)
+                {
+                    Send_to_client(FTYPE.STOP_REQ, FLAG.NONE, null);
+                    return false;
+                }
                 Array.Clear(buf_radiotap_hdr, 0, buf_radiotap_hdr.Length);
                 clientSocket.Receive(buf_radiotap_hdr, (int)len_radiotap, SocketFlags.None);
                 Write_to_logfile(buf_radiotap_hdr);
 
                 // IEEE80211 frame
-                Wait_For_Datastream((int)(R_length - len_radiotap), "IEEE80211 packet " + packet_nr.ToString());
+                wait_flag = Wait_For_Datastream((int)(R_length - len_radiotap), "IEEE80211 packet " + packet_nr.ToString());
+                if (wait_flag == 1)
+                {
+                    Send_to_client(FTYPE.ERROR, FLAG.NONE, null);
+                    return true;
+                }
+                else if (wait_flag == 2)
+                {
+                    Send_to_client(FTYPE.STOP_REQ, FLAG.NONE, null);
+                    return false;
+                }
                 Array.Clear(buf_ieee80211_frame, 0, buf_ieee80211_frame.Length);
                 clientSocket.Receive(buf_ieee80211_frame, (int)(R_length - len_radiotap), SocketFlags.None);
                 Write_to_logfile(buf_ieee80211_frame);
                 
-                // send ack data
+                // send ack data, send stop request instead if STOP key pressed
                 if(Console.KeyAvailable == true)
                 {
                     ConsoleKeyInfo cki = Console.ReadKey(true);
@@ -568,20 +659,32 @@ namespace Sniffing
                 Console.WriteLine("packet {0}\tlength {1}\t{2}\n", ++packet_nr, R_length, Get_ieee80211_type(BitConverter.ToUInt16(buf_ieee80211_frame, 0)));
                 //Console.WriteLine("radiotap header:");
                 //Print_Content(buf_radiotap_hdr, 0, len_radiotap);
-                Console.WriteLine("MAC header:");
-                Print_Content(buf_ieee80211_frame, 0, len_MAC);
+                Console.WriteLine("Radiotap Packet:");
+                Print_Content(buf_radiotap_hdr, 0, len_radiotap, ConsoleColor.Blue);
+                Console.WriteLine("\nMAC header:");
+                Print_Content(buf_ieee80211_frame, 0, len_MAC, ConsoleColor.Green);
                 Console.WriteLine("\nframe body:");
-                Print_Content(buf_ieee80211_frame, len_MAC, len_MAC+len_frame_body);
+                Print_Content(buf_ieee80211_frame, len_MAC, len_MAC+len_frame_body, ConsoleColor.Cyan);
                 Console.WriteLine("\nFCS:");
-                Print_Content(buf_ieee80211_frame, len_MAC+len_frame_body-4, len_MAC+len_frame_body);
+                Print_Content(buf_ieee80211_frame, len_MAC+len_frame_body, len_MAC+len_frame_body+4, ConsoleColor.Yellow);
             }
             else
             {
                 Array.Clear(BUFFER.receive, 0, BUFFER.receive.Length);
-                Wait_For_Datastream(R_length, "windows-linux control frame");
+                wait_flag = Wait_For_Datastream(R_length, "windows-linux control frame");
+                if (wait_flag == 1)
+                {
+                    Send_to_client(FTYPE.ERROR, FLAG.NONE, null);
+                    return true;
+                }
+                else if (wait_flag == 2)
+                {
+                    Send_to_client(FTYPE.STOP_REQ, FLAG.NONE, null);
+                    return false;
+                }
                 clientSocket.Receive(BUFFER.receive, R_length, SocketFlags.None);
             }
-            Thread.Sleep(500);
+            Thread.Sleep(100);
             return true;
         }
         /*
@@ -642,7 +745,7 @@ namespace Sniffing
             while (true)
             {
                 int num = sock.Receive(buf_receive, sock.Available, SocketFlags.None);
-                Print_Content(buf_receive, 0, num);
+                //Print_Content(buf_receive, 0, num);
                 while (Console.KeyAvailable == false)
                 {
 
